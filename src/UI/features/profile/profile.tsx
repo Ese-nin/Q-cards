9bf9dbc0ccc2449ff7bb47579d0ab27457c58e61
Evseev Anton <soki1997@mail.ru>
2023-01-07 16:59:03 +0500

    add: mui and log out btn

--- src/UI/features/profile/profile.tsx
+++ src/UI/features/profile/profile.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import {useAppSelector} from "../../../bll/store";
-import {Navigate} from "react-router-dom";
+import {useAppSelector} from '../../../bll/store';
+import {Navigate} from 'react-router-dom';
+import avatar from './Ellipse 45.png'
+import s from './profile.module.css'
+import {Button} from '@mui/material';
+import {Logout} from '@mui/icons-material';
 
 export const Profile = () => {
 
     const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
 
-    if (!isLoggedIn) {
-        return <Navigate to={'/login'}/>
-    }
+    // if (!isLoggedIn) {
+    //     return <Navigate to={'/login'}/>
+    // }
 
     return (
-        <div>
-            PROFILE PAGE
+        <div className={s.profileBlock}>
+            Внимание! Проверка на залоггированность закоменчена для рабочего процесса!
+            <span>Personal Information</span>
+            <img src={avatar} alt="avatar" className={s.imgAvatar}/>
+            <div>
+                <span>Ivan</span>
+                <button>change name</button>
+            </div>
+            <span>j&johnson@gmail.com</span>
+            <Button  variant="outlined" startIcon={<Logout/>}>
+                Log out
+            </Button>
         </div>
     );
 };
