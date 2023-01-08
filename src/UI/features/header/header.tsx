c5d4093c93c090b09ebfb8287e91945f1ad554d1
Evseev Anton <soki1997@mail.ru>
2023-01-08 12:39:24 +0500

    feat: app bar from mui and some css style

--- src/UI/features/header/header.tsx
+++ src/UI/features/header/header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import s from './header.module.css'
+import MenuAppBar from './AppBarMui';
 
 export const Header = () => {
     return (
         <div>
             <div className={s.item}>
                 <NavLink to='/profile'
-                         className={({isActive}) => (isActive ? s.active : "")}>Profile</NavLink>
+                         className={({isActive}) => (isActive ? s.active : "")}><MenuAppBar/></NavLink>
             </div>
         </div>
     );
