16382c2234fc75012746989b2b636ccae9b8ea36
Evseev Anton <soki1997@mail.ru>
2023-01-22 17:44:39 +0500

    add: AddNewPackModal.tsx and css

--- src/UI/features/cards/PacksList.tsx
+++ src/UI/features/cards/PacksList.tsx
@@ -1,129 +1,127 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "bll/store";
-import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
-import s from "./packList.module.css";
-import s2 from "./tables/TablesPackList.module.css";
-import FilterAltOffIcon from "@mui/icons-material/FilterAltOff";
-import { addNewCardPackTC, getCardsPackTC } from "bll/reducers/packs-reducer";
-import { ChoiceCards } from "./ChoiceCards/ChoiceCards";
-import { RangeSlider } from "./RangeSlider/RangeSlider";
-import { SearchInput } from "./SearchInput/SearchInput";
-import { PATH } from "bll/Path";
-import TablesPackList from "./tables/TablesPackList";
-import { useDebounce } from "utils/hooks/useDebounce";
+import React, {useEffect, useState} from 'react';
+import {useAppDispatch, useAppSelector} from 'bll/store';
+import {Navigate, useNavigate, useSearchParams} from 'react-router-dom';
+import s from './packList.module.css';
+import s2 from './tables/TablesPackList.module.css';
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
+import {addNewCardPackTC, getCardsPackTC} from 'bll/reducers/packs-reducer';
+import {ChoiceCards} from './ChoiceCards/ChoiceCards';
+import {RangeSlider} from './RangeSlider/RangeSlider';
+import {SearchInput} from './SearchInput/SearchInput';
+import {PATH} from 'bll/Path';
+import TablesPackList from './tables/TablesPackList';
+import {useDebounce} from 'utils/hooks/useDebounce';
 import {
-  cardPacksTotalCountSelector,
-  isLoggedInSelector,
-  maxCardsCountSelector,
-  minCardsCountSelector,
-  pageCountPacksSelector,
-  pagePacksSelector,
-  user_idSelector,
-} from "bll/selectors";
-import { SuperButton, SuperPagination } from "UI/common";
+    cardPacksTotalCountSelector,
+    isLoggedInSelector,
+    maxCardsCountSelector,
+    minCardsCountSelector,
+    pageCountPacksSelector,
+    pagePacksSelector,
+    user_idSelector,
+} from 'bll/selectors';
+import {SuperButton, SuperPagination} from 'UI/common';
+import {AddNewPackModal} from '../../../components/modal/AddNewPackModal';
 
 export const PackList = () => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const isLoggedIn = useAppSelector(isLoggedInSelector);
-  const page = useAppSelector(pagePacksSelector);
-  const pageCount = useAppSelector(pageCountPacksSelector);
-  const cardPacksTotalCount = useAppSelector(cardPacksTotalCountSelector);
-  const minCardsCount = useAppSelector(minCardsCountSelector);
-  const maxCardsCount = useAppSelector(maxCardsCountSelector);
-  const userID = useAppSelector(user_idSelector);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const isLoggedIn = useAppSelector(isLoggedInSelector);
+    const page = useAppSelector(pagePacksSelector);
+    const pageCount = useAppSelector(pageCountPacksSelector);
+    const cardPacksTotalCount = useAppSelector(cardPacksTotalCountSelector);
+    const minCardsCount = useAppSelector(minCardsCountSelector);
+    const maxCardsCount = useAppSelector(maxCardsCountSelector);
+    const userID = useAppSelector(user_idSelector);
 
-  const [searchParams, setSearchParams]: [URLSearchParams, Function] = useSearchParams();
-  const params = Object.fromEntries(searchParams);
+    const [searchParams, setSearchParams]: [URLSearchParams, Function] = useSearchParams();
+    const params = Object.fromEntries(searchParams);
 
-  const [values, setValues] = useState([minCardsCount, maxCardsCount]);
-  const [find, setFind] = useState("");
+    const [values, setValues] = useState([minCardsCount, maxCardsCount]);
+    const [find, setFind] = useState('');
 
-  const sliderDebouncedValue = useDebounce<number[]>(values, 600);
-  const searchDebouncedValue = useDebounce<string>(find, 600);
+    const sliderDebouncedValue = useDebounce<number[]>(values, 600);
+    const searchDebouncedValue = useDebounce<string>(find, 600);
 
-  const changeSliderValues = (event: Event, value: number | number[]) => {
-    if (Array.isArray(value)) {
-      setValues(value);
+    const changeSliderValues = (event: Event, value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValues(value);
+        }
+    };
+
+    useEffect(() => {
+        // dispatch(getCardsPackTC({...params, min: values[0], max: values[1]}))
+        setSearchParams({...params, min: values[0], max: values[1]});
+    }, [sliderDebouncedValue]);
+
+    const onChangeText = (value: string) => {
+        setFind(value);
+    };
+
+    useEffect(() => {
+        // dispatch(getCardsPackTC({...params, packName: find}))
+        setSearchParams({...params, packName: find});
+    }, [searchDebouncedValue]);
+
+    const addNewCardsPack = () => {
+        params.user_id ? dispatch(addNewCardPackTC({}, userID)) : dispatch(addNewCardPackTC({}));
+    };
+
+    const onChangePagination = (newPage: number, newCount: number) => {
+        dispatch(getCardsPackTC({...params, page: newPage, pageCount: newCount}));
+        setSearchParams({...params, page: newPage, pageCount: newCount});
+    };
+
+    const resetFilter = () => {
+        dispatch(getCardsPackTC({}));
+    };
+
+    if (!isLoggedIn) {
+        return <Navigate to={PATH.LOGIN}/>;
     }
-  };
 
-  useEffect(() => {
-    // dispatch(getCardsPackTC({...params, min: values[0], max: values[1]}))
-    setSearchParams({ ...params, min: values[0], max: values[1] });
-  }, [sliderDebouncedValue]);
+    return (
+        <div className={s.page}>
+            <div className={s.addNewPackLine}>
+                <div>
+                    <h2>Packs list</h2>
+                </div>
+                <AddNewPackModal/>
+            </div>
+            <div className={s.formLine}>
+                <div className={s.searchField}>
+                    <SearchInput value={find} onChangeText={onChangeText} placeholder={'Search'}/>
+                </div>
+                <div>
+                    <ChoiceCards userID={userID}/>
+                </div>
+                <div className={s.slider}>
+                    <span>{values[0]}</span>
+                    <RangeSlider
+                        min={minCardsCount}
+                        max={maxCardsCount}
+                        value={values}
+                        onChange={changeSliderValues}
+                    />
+                    <span>{values[1]}</span>
+                </div>
+                <SuperButton className={s2.button_style} onClick={resetFilter}>
+                    <FilterAltOffIcon/>
+                </SuperButton>
+            </div>
 
-  const onChangeText = (value: string) => {
-    setFind(value);
-  };
+            <div className={s.tableBlock}>
+                <TablesPackList/>
+            </div>
 
-  useEffect(() => {
-    // dispatch(getCardsPackTC({...params, packName: find}))
-    setSearchParams({ ...params, packName: find });
-  }, [searchDebouncedValue]);
-
-  const addNewCardsPack = () => {
-    params.user_id ? dispatch(addNewCardPackTC({}, userID)) : dispatch(addNewCardPackTC({}));
-  };
-
-  const onChangePagination = (newPage: number, newCount: number) => {
-    dispatch(getCardsPackTC({ ...params, page: newPage, pageCount: newCount }));
-    setSearchParams({ ...params, page: newPage, pageCount: newCount });
-  };
-
-  const resetFilter = () => {
-    dispatch(getCardsPackTC({}));
-  };
-
-  if (!isLoggedIn) {
-    return <Navigate to={PATH.LOGIN} />;
-  }
-
-  return (
-    <div className={s.page}>
-      <div className={s.addNewPackLine}>
-        <div>
-          <h2>Packs list</h2>
+            <div className={s.pagination}>
+                <SuperPagination
+                    page={page}
+                    itemsCountForPage={pageCount}
+                    totalCount={cardPacksTotalCount}
+                    onChange={onChangePagination}
+                />
+            </div>
         </div>
-        <Button variant="outlined" onClick={addNewCardsPack}>
-          Add new pack
-        </Button>
-      </div>
-      <div className={s.formLine}>
-        <div className={s.searchField}>
-          <SearchInput value={find} onChangeText={onChangeText} placeholder={"Search"} />
-        </div>
-        <div>
-          <ChoiceCards userID={userID} />
-        </div>
-        <div className={s.slider}>
-          <span>{values[0]}</span>
-          <RangeSlider
-            min={minCardsCount}
-            max={maxCardsCount}
-            value={values}
-            onChange={changeSliderValues}
-          />
-          <span>{values[1]}</span>
-        </div>
-        <SuperButton className={s2.button_style} onClick={resetFilter}>
-          <FilterAltOffIcon />
-        </SuperButton>
-      </div>
-
-      <div className={s.tableBlock}>
-        <TablesPackList />
-      </div>
-
-      <div className={s.pagination}>
-        <SuperPagination
-          page={page}
-          itemsCountForPage={pageCount}
-          totalCount={cardPacksTotalCount}
-          onChange={onChangePagination}
-        />
-      </div>
-    </div>
-  );
+    );
 };
---
+++ src/components/modal/BasicModals.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import {FC, ReactNode} from 'react';
+import {Button} from '@mui/material';
+
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 347,
+    height: 311,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    padding: 3,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+};
+
+type PropsType = {
+    children: ReactNode
+}
+
+
+export const BasicModal: FC<PropsType> = ({children}) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <Button onClick={handleOpen} variant='contained'>Add new pack</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <Box sx={style}>
+                    {children}
+                </Box>
+            </Modal>
+        </div>
+    );
+}
