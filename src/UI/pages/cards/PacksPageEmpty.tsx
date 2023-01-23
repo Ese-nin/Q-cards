753fd3f46789a5753cf4646733fb5487666ed93d
Сергей <privet15.89@mail.ru>
2023-01-23 23:16:53 +0500

    fix: set redirect to cards if cardsCount > 0

--- src/UI/pages/cards/PacksPageEmpty.tsx
+++ src/UI/pages/cards/PacksPageEmpty.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "../../../bll/store";
+import { useAppDispatch, useAppSelector } from "bll/store";
 import s from "./packList.module.css";
-import { PATH } from "../../../bll/Path";
-import { addNewCardTC } from "../../../bll/reducers/cards-reducer";
-import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
-import { isLoggedInSelector, packNameSelector } from "../../../bll/selectors";
-import back from "../../../assets/icon/back.svg";
-import { AddNewCardModal } from "../../../components/modal/AddNewCardModal";
+import { PATH } from "bll/Path";
+import { addNewCardTC } from "bll/reducers/cards-reducer";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { cardsTotalCountSelector, isLoggedInSelector, packNameSelector } from "bll/selectors";
+import back from "assets/icon/back.svg";
+import { AddNewCardModal } from "components/modal/AddNewCardModal";
 
 export const PackPageEmpty = () => {
   const dispatch = useAppDispatch();
   const isLoggedIn = useAppSelector(isLoggedInSelector);
   const packName = useAppSelector(packNameSelector);
+  const totalCardsCount = useAppSelector(cardsTotalCountSelector);
   const navigate = useNavigate();
   const [searchParams, setSearchParams]: [URLSearchParams, Function] = useSearchParams();
   const params = Object.fromEntries(searchParams);
   const cardsPack_id = params.cardsPack_id;
+
+  const [cardsCount] = useState<number>(totalCardsCount);
 
   const addNewCard = (question: string) => {
     dispatch(addNewCardTC({ cardsPack_id, question }));
@@ -24,7 +27,11 @@ export const PackPageEmpty = () => {
   };
 
   if (!isLoggedIn) {
-    return <Navigate to={PATH.LOGIN} />;
+    navigate(PATH.LOGIN);
+  }
+
+  if (!cardsCount) {
+    navigate(PATH.PACK_PAGE + "?cardsPack_id=" + cardsPack_id);
   }
 
   const BackToPackList = () => {
