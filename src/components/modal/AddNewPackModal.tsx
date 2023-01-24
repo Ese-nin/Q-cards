d91d15799fa14856901005feaec3ba87e255f8a9
Evseev Anton <soki1997@mail.ru>
2023-01-24 18:57:07 +0500

    fix: AddNewPackModal.tsx

--- src/components/modal/AddNewPackModal.tsx
+++ src/components/modal/AddNewPackModal.tsx
@@ -1,37 +1,56 @@
 import {BasicModal} from './BasicModals';
-import {SuperCheckbox} from '../../UI/common';
-import closeBtn from '../../assets/icon/closeBtn.svg'
 import s from './Modals.module.css'
 import TextField from '@mui/material/TextField';
-import {Button} from '@mui/material';
+import {Button, Checkbox} from '@mui/material';
+import * as React from 'react';
+
 
 export const AddNewPackModal = () => {
+
+    const Title = 'Add new pack'
+
+// для инпута
+    const [namePack, setNamePack] = React.useState<string>('')
+    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNamePack(e.target.value)
+    }
+
+// для чекбокса
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
+
 
 
 
     return (
         <BasicModal>
             <div className={s.firstBlock}>
-                <span>Add new pack</span>
-                <button >
-                    <img src={closeBtn} alt="close"/>
-                </button>
+                <span>{Title}</span>
             </div>
-            {/*<hr/>*/}
             <div>
-                <TextField id="standard-basic" label="Name pack" variant="standard" sx={{width: '347px'}}/>
+                <TextField
+                    id="standard-basic"
+                    label="Name pack"
+                    variant="standard"
+                    sx={{width: '347px'}}
+                    onChange={handleChangeCheckbox}
+                />
             </div>
             <div className={s.checkBoxBlock}>
-                <SuperCheckbox/>
+                <Checkbox
+                    checked={checked}
+                    onChange={handleChangeCheckbox}
+                    inputProps={{'aria-label': 'controlled'}}
+                />
                 <span>Private pack</span>
             </div>
             <div className={s.saveBlock}>
                 <Button variant="outlined">Cancel</Button>
                 <Button variant="contained">Save</Button>
             </div>
-
-
         </BasicModal>
-
     )
 }
