3a68a1c117b2aadec0ee5a2fa9d50dd9726fc130
Сергей <privet15.89@mail.ru>
2023-01-26 03:00:10 +0500

    fix: set view button on empty page

--- src/UI/pages/cards/PacksPageEmpty.tsx
+++ src/UI/pages/cards/PacksPageEmpty.tsx
@@ -1,7 +1,6 @@
-import { useAppDispatch, useAppSelector } from "bll/store";
+import { useAppSelector } from "bll/store";
 import s from "./packList.module.css";
 import { PATH } from "bll/Path";
-import { addNewCardTC } from "bll/reducers/cards-reducer";
 import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
 import {
   cardsTotalCountSelector,
@@ -12,11 +11,8 @@ import {
 } from "bll/selectors";
 import back from "assets/icon/back.svg";
 import { AddNewCardModal } from "components/modal/AddNewCardModal";
-import { Button } from "@mui/material";
 
 export const PackPageEmpty = () => {
-  const dispatch = useAppDispatch();
-
   const isLoggedIn = useAppSelector(isLoggedInSelector);
   const packName = useAppSelector(packNameSelector);
   const totalCardsCount = useAppSelector(cardsTotalCountSelector);
@@ -25,18 +21,13 @@ export const PackPageEmpty = () => {
 
   const navigate = useNavigate();
 
-  const [searchParams, setSearchParams]: [URLSearchParams, Function] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const params = Object.fromEntries(searchParams);
   const cardsPack_id = params.cardsPack_id;
 
-    const addNewCard = (question: string) => {
-        dispatch(addNewCardTC({cardsPack_id, question}));
-        setSearchParams({...params, cardsPack_id, cardQuestion: question});
-    };
-
-    if (!isLoggedIn) {
-        return <Navigate to={PATH.LOGIN}/>;
-    }
+  if (!isLoggedIn) {
+    return <Navigate to={PATH.LOGIN} />;
+  }
 
   if (!isLoggedIn) {
     navigate(PATH.LOGIN);
@@ -52,26 +43,26 @@ export const PackPageEmpty = () => {
 
   const viewButton = meID === packUserID;
 
-
-    return (
-        <div className={s.page}>
-            <div className={s.backBlock} onClick={BackToPackList}>
-                <img src={back} alt="back"/>
-                <span>Back to Packs List</span>
-            </div>
-            <div className={s.addNewPackLine}>
-                <div className={s.nameAndBurger}>
-                    <h2>{packName}</h2>
-                </div>
-            </div>
-            <div className={s.emptyCenterBlock}>
-                <span>This pack is empty. Click add new card to fill this pack</span>
-                <Button variant="outlined" onClick={() => addNewCard('New question')}>
-                    Add New Card
-                </Button>
-                эта кнопка для модалки
-                <AddNewCardModal cardsPackId={cardsPack_id}/>
-            </div>
+  return (
+    <div className={s.page}>
+      <div className={s.backBlock} onClick={BackToPackList}>
+        <img src={back} alt="back" />
+        <span>Back to Packs List</span>
+      </div>
+      <div className={s.addNewPackLine}>
+        <div className={s.nameAndBurger}>
+          <h2>{packName}</h2>
         </div>
-    );
+      </div>
+      <div className={s.emptyCenterBlock}>
+        <span>This pack is empty.</span>
+        {viewButton && (
+          <>
+            <span>Click add new card to fill this pack</span>
+            <AddNewCardModal cardsPackId={cardsPack_id} />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
