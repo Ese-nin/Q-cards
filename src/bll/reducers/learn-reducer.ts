134808dcf2a489c12229491831b50416c59046a6
Leemelioon <kotxbossx@gmail.com>
2023-01-25 20:36:11 +0300

    Kirill: rollback

---
+++ src/bll/reducers/learn-reducer.ts
@@ -0,0 +1,38 @@
+import { AppThunk } from "../store";
+import { setAppStatusAC } from "./app-reducer";
+import axios from "axios";
+import { learnAPI, UpdatedGradeType } from "../../dal/learnAPI";
+
+export type InitialLearnStateType = UpdatedGradeType;
+
+export type LearnActionType = PutAGradeACType;
+export const learnReducer = (
+  state: InitialLearnStateType,
+  action: LearnActionType
+): InitialLearnStateType => {
+  switch (action.type) {
+    case "PUT_A_GRAGE":
+      return {
+        ...state,
+        ...action,
+      };
+  }
+};
+
+export const putAGradeAC = (card: UpdatedGradeType) =>
+  ({
+    type: "PUT_A_GRAGE",
+    card,
+  } as const);
+
+export type PutAGradeACType = ReturnType<typeof putAGradeAC>;
+
+export const putAGradeTC =
+  (grade: number, card_id: string): AppThunk =>
+  (dispatch) => {
+    dispatch(setAppStatusAC("loading"));
+    learnAPI.putAGrade(grade, card_id).then((res) => {
+      /*dispatch(putAGradeAC(res.data));*/
+      dispatch(setAppStatusAC("succeeded"));
+    });
+  };
---
+++ src/dal/learnAPI.ts
@@ -0,0 +1,25 @@
+import { AxiosResponse } from "axios";
+import { instance } from "./instance";
+
+export const learnAPI = {
+  putAGrade(grade: number, card_id: string) {
+    return instance.put<{}, AxiosResponse<UpdatedGradeType>, PutAGradeType>("cards/grade", {
+      grade,
+      card_id,
+    });
+  },
+};
+
+type PutAGradeType = {
+  grade: number;
+  card_id: string;
+};
+
+export type UpdatedGradeType = {
+  _id: string;
+  cardsPack_id: string;
+  card_id: string;
+  user_id: string;
+  grade: number;
+  shots: number;
+};
