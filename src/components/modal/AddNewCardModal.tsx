31cdc2853308f71352ecc14454cec3e46b44b17a
Evseev Anton <soki1997@mail.ru>
2023-01-25 07:45:13 +0500

    add: AddNewCardModal.tsx

--- src/components/modal/AddNewCardModal.tsx
+++ src/components/modal/AddNewCardModal.tsx
@@ -4,14 +4,16 @@ import s from './Modals.module.css'
 import TextField from '@mui/material/TextField';
 import {Button} from '@mui/material';
 import * as React from 'react';
+import {addNewCardTC} from '../../bll/reducers/cards-reducer';
+import {useAppDispatch} from '../../bll/store';
 
 
 export const AddNewCardModal = () => {
+    const dispatch = useAppDispatch();
 
-    const Title = 'Edit card'
+    const Title = 'Add new card'
 
-
-
+// чекбокс --------------------------------------------
     const TextOrImg = [
         {id: 1, value: 'Text'},
         {id: 2, value: 'Image'},
@@ -20,7 +22,25 @@ export const AddNewCardModal = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+// -------------------------------------------------------------
 
+    const addNewCard = (question: string) => {
+        dispatch(addNewCardTC({cardsPack_id, question}));
+        setSearchParams({...params, cardsPack_id, cardQuestion: question});
+    };
+
+
+    // для инпута---------------------------------------------------------
+    const [Question, setQuestion] = React.useState<string>('')
+    const handleChangeQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQuestion(e.target.value)
+    }
+
+    const [Answer, setAnswer] = React.useState<string>('')
+    const handleChangeAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(e.target.value)
+    }
+    //-----------------------------------------------------------
 
     return (
         <>
@@ -41,8 +61,28 @@ export const AddNewCardModal = () => {
                 </div>
 
                 <div>
-                    <TextField id="standard-basic" label="Question" variant="standard" sx={{width: '347px'}}/>
-                    <TextField id="standard-basic" label="Answer" variant="standard" sx={{width: '347px'}}/>
+                    <TextField id="Question"
+                               label="Question"
+                               variant="standard"
+                               sx={{width: '347px'}}
+                               onChange={handleChangeQuestion}
+                               value={Question}
+                    />
+                    <TextField id="Answer"
+                               label="Answer"
+                               variant="standard"
+                               sx={{width: '347px'}}
+                               onChange={handleChangeAnswer}
+                               value={Answer}
+                    />
+                </div>
+                <div className={s.saveBlock}>
+                    <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+                    <Button variant="contained"
+                            onClick={() => addNewCard('New question')}
+                    >
+                        Save
+                    </Button>
                 </div>
             </BasicModal>
         </>
