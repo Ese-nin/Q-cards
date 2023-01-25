57a1de0905a4ae76c356cb7c5c4952fb5e24a2c5
Evseev Anton <soki1997@mail.ru>
2023-01-25 21:00:32 +0500

    add: DeleteCardModal.tsx

--- src/components/modal/AddNewCardModal.tsx
+++ src/components/modal/AddNewCardModal.tsx
@@ -1,97 +1,93 @@
-import {BasicModal} from './BasicModals';
-import {SuperSelect} from '../../UI/common';
-import s from './Modals.module.css'
-import TextField from '@mui/material/TextField';
-import {Button} from '@mui/material';
-import * as React from 'react';
-import {addNewCardTC} from '../../bll/reducers/cards-reducer';
-import {useAppDispatch} from '../../bll/store';
+import { BasicModal } from "./BasicModals";
+import { SuperSelect } from "../../UI/common";
+import s from "./Modals.module.css";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import * as React from "react";
+import { addNewCardTC } from "../../bll/reducers/cards-reducer";
+import { useAppDispatch } from "../../bll/store";
 
 type PropsType = {
-    cardsPackId: string
-}
+  cardsPackId: string;
+};
 export const AddNewCardModal = (props: PropsType) => {
-    const dispatch = useAppDispatch();
-    const cardsPack_id = props.cardsPackId
+  const dispatch = useAppDispatch();
+  const cardsPack_id = props.cardsPackId;
 
-    const Title = 'Add new card'
+  const Title = "Add new card";
 
-// чекбокс --------------------------------------------
-    const TextOrImg = [
-        {id: 1, value: 'Text'},
-        {id: 2, value: 'Image'},
-    ]
+  // чекбокс --------------------------------------------
+  const TextOrImg = [
+    { id: 1, value: "Text" },
+    { id: 2, value: "Image" },
+  ];
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-// -------------------------------------------------------------
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  // -------------------------------------------------------------
 
+  // для инпута---------------------------------------------------------
+  const [Question, setQuestion] = React.useState<string>("");
+  const handleChangeQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value);
+  };
 
-    // для инпута---------------------------------------------------------
-    const [Question, setQuestion] = React.useState<string>('')
-    const handleChangeQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuestion(e.target.value)
-    }
+  const [Answer, setAnswer] = React.useState<string>("");
+  const handleChangeAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAnswer(e.target.value);
+  };
+  //-----------------------------------------------------------
 
-    const [Answer, setAnswer] = React.useState<string>('')
-    const handleChangeAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAnswer(e.target.value)
-    }
-    //-----------------------------------------------------------
+  const addNewCard = (Question: string, Answer: string) => {
+    dispatch(addNewCardTC({ cardsPack_id, question: Question, answer: Answer }));
+    // setSearchParams({...params, cardsPack_id, cardQuestion: Question});
+    handleClose();
+  };
 
-    const addNewCard = (Question: string, Answer: string) => {
-        dispatch(addNewCardTC({cardsPack_id, question: Question, answer: Answer}));
-        // setSearchParams({...params, cardsPack_id, cardQuestion: Question});
-        console.log(cardsPack_id)
-    };
+  return (
+    <>
+      <Button onClick={handleOpen} variant="contained">
+        {Title}
+      </Button>
 
+      <BasicModal open={open} handleClose={handleClose}>
+        <div className={s.firstBlock}>
+          <span>{Title}</span>
+        </div>
 
-    return (
-        <>
-            <Button onClick={handleOpen} variant="contained">
-                {Title}
-            </Button>
+        <div>
+          <span>Choose a question format</span>
+          <SuperSelect options={TextOrImg} />
+        </div>
 
-            <BasicModal open={open} handleClose={handleClose}>
-                <div className={s.firstBlock}>
-                    <span>{Title}</span>
-                </div>
-
-                <div>
-                    <span>Choose a question format</span>
-                    <SuperSelect
-                        options={TextOrImg}
-                    />
-                </div>
-
-                <div>
-                    <TextField id="Question"
-                               label="Question"
-                               variant="standard"
-                               sx={{width: '347px'}}
-                               onChange={handleChangeQuestion}
-                               value={Question}
-                    />
-                    <TextField id="Answer"
-                               label="Answer"
-                               variant="standard"
-                               sx={{width: '347px'}}
-                               onChange={handleChangeAnswer}
-                               value={Answer}
-                    />
-                </div>
-                <div className={s.saveBlock}>
-                    <Button variant="outlined" onClick={handleClose}>Cancel</Button>
-                    <Button variant="contained"
-                            onClick={() => addNewCard(Question, Answer)}
-                    >
-                        Save
-                    </Button>
-                </div>
-            </BasicModal>
-        </>
-
-
-    )
-}
+        <div>
+          <TextField
+            id="Question"
+            label="Question"
+            variant="standard"
+            sx={{ width: "347px" }}
+            onChange={handleChangeQuestion}
+            value={Question}
+          />
+          <TextField
+            id="Answer"
+            label="Answer"
+            variant="standard"
+            sx={{ width: "347px" }}
+            onChange={handleChangeAnswer}
+            value={Answer}
+          />
+        </div>
+        <div className={s.saveBlock}>
+          <Button variant="outlined" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={() => addNewCard(Question, Answer)}>
+            Save
+          </Button>
+        </div>
+      </BasicModal>
+    </>
+  );
+};
---
+++ src/components/modal/DeleteCardModal.tsx
@@ -0,0 +1,61 @@
+import { BasicModal } from "./BasicModals";
+import s from "./Modals.module.css";
+import { Button } from "@mui/material";
+import { useAppDispatch } from "../../bll/store";
+import * as React from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { deleteCardTC } from "../../bll/reducers/cards-reducer";
+
+type PropsType = {
+  id: string;
+  name: string;
+  cardsPack_id: string;
+};
+
+export const DeleteCardModal = (props: PropsType) => {
+  const dispatch = useAppDispatch();
+
+  const id = props.id;
+  const Name = props.name;
+  const cardsPackId = props.cardsPack_id;
+  const Title = "Delete Card";
+
+  const removeCard = (cardsPackId: string, card_id: string) => {
+    dispatch(deleteCardTC(cardsPackId, card_id));
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={handleOpen} variant="contained" className={s.button_style}>
+        <DeleteOutlineIcon className={s.icon_style} />
+      </Button>
+
+      <BasicModal open={open} handleClose={handleClose}>
+        <div className={s.firstBlock}>
+          <span>{Title}</span>
+        </div>
+
+        <div>
+          Do you really want to remove <span style={{ fontWeight: "bold" }}>{Name}</span>? All cards
+          will be deleted.
+        </div>
+        <div className={s.saveBlock}>
+          <Button variant="outlined" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "red" }}
+            onClick={() => removeCard(cardsPackId, id)}
+          >
+            Delete
+          </Button>
+        </div>
+      </BasicModal>
+    </>
+  );
+};
